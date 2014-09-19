ba6f65378067d74cc57bdbae5d1ec7aaae4b5530
Peter Mooshammer <pmoosh@yahoo.com>
2014-09-19 15:51:37 -0700

    added nginx logging

--- recipes/default.rb
+++ recipes/default.rb
@@ -45,3 +45,13 @@ cookbook_file '/etc/pki/tls/certs/logstash.crt' do
 end
 
 include_recipe 'rsyslog::client'
+
+cookbook_file '/etc/rsyslog.d/21-nginx.conf' do
+  source '21-nginx.conf'
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :restart, "service[#{node['rsyslog']['service_name']}]"
+end
+
+
