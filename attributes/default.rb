84c68beb8b76ed92d9f781cfb6f28175d09f5928
Peter Mooshammer <pmoosh@yahoo.com>
2014-09-29 16:04:47 -0700

    missed to remove duplicated attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,27 +2,3 @@ default['eas-base']['ops_group'] = 'sysadmin'
 default['eas-base']['dev_group'] = 'dev'
 default['eas-base']['apps_group'] = 'www-data'
 default['eas-base']['all_users'] = [node['eas-base']['ops_group'], node['eas-base']['dev_group'], node['eas-base']['apps_group']]
-
-default['authorization']['sudo']['groups'] = [node['eas-base']['ops_group']]
-default['authorization']['sudo']['passwordless'] = true
-default['authorization']['sudo']['users']  = %w(ubuntu vagrant)
-
-# logstash server port
-normal['rsyslog']['server_search'] = 'role:loghost'
-normal['rsyslog']['port'] = 5959
-normal['rsyslog']['enable_tls'] = true
-normal['rsyslog']['tls_ca_file'] = '/etc/pki/tls/certs/logstash.crt'
-
-normal['postfix']['main']['smtp_use_tls'] = 'yes'
-normal['postfix']['main']['smtp_tls_CAfile'] = node['rsyslog']['tls_ca_file']
-normal['postfix']['main']['smtp_tls_session_cache_database'] = 'btree:${data_directory}/smtp_scache'
-
-normal['apt']['compile_time_update'] = true
-# parameters used for route53
-default['eas-base']['hosted_domain'] = 'eas.promethost.com'
-default['eas-base']['zone_id'] = 'Z17UO48EJODEL9'
-
-# chef-client attributes
-normal['chef_client']['init_style'] = 'none'
-normal['chef_client']['cron']['minute'] = '*/20'
-normal['chef_client']['cron']['hour'] = '*'
