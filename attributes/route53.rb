de28ba7fb1c9c025ecfd9ccd8f89605f6b3d5889
Peter Mooshammer <pmoosh@yahoo.com>
2014-12-22 16:46:56 -0800

    fixed error with missing default values for zone_id and hosted_domain

---
+++ attributes/route53.rb
@@ -0,0 +1,3 @@
+# parameters used for route53
+default['route53']['hosted_domain'] = 'eas.promethost.com'
+default['route53']['zone_id'] = 'Z17UO48EJODEL9'
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscale.com'
 license 'Apache 2.0'
 description 'Installs/Configures eas-base'
 long_description 'Installs/Configures eas-base'
-version '0.1.10'
+version '0.1.12'
 
 depends 'apt', '~> 2.6.0'
 depends 'chef-solo-search', '~> 0.5.1'
